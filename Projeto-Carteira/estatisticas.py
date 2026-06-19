5b6d27689814c2f61b7710a1a83e4e6f1e0e2094
Gabriel Koch <gabrielkochsilva@gmail.com>
2026-06-18 22:08:33 -0300

    Add files via upload

---
+++ Projeto-Carteira/estatisticas.py
@@ -0,0 +1,56 @@
+import utils as u
+
+def media_gastos():
+    u.read_key()
+    pass
+def maior_menor_despesa():
+    u.read_key()
+    pass
+def maior_menor_entrada():
+    u.read_key()
+    pass
+def desvio_padrao():
+    u.read_key()
+    pass
+def percentual_categoria():
+    u.read_key()
+    pass
+
+
+def menu_estatisticas():
+    while True:
+        u.limpar_tela()
+        u.line()
+        print('ESTATÍSTICAS'.center(30,' '))
+        u.line()
+        print('1 - MÉDIA DE GASTOS')
+        print('2 - MAIOR E MENOR DESPESA')
+        print('3 - MAIOR E MENOR ENTRADA')
+        print('4 - DESVIO PADRÃO')
+        print('5 - PERCENTUAL POR CATEGORIA')
+        print('0 - VOLTAR')
+        u.line()
+        opcao = input('Digite a opção desejada: ')
+        u.line()
+        if opcao == '1':
+            media_gastos()
+        elif opcao == '2':
+            maior_menor_despesa()
+        elif opcao == '3':
+            maior_menor_entrada()
+        elif opcao == '4':
+            desvio_padrao()
+        elif opcao == '5':
+            percentual_categoria()
+        elif opcao == '0':
+            u.limpar_tela()
+            break
+        else:
+            u.limpar_tela()
+            print('Opcão Inválida')
+            u.read_key()
+
+
+if __name__ == '__main__':
+    menu_estatisticas()
+
